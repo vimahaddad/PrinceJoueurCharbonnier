96ca09d4c5b62353a148b9e1756def031a103040
Vima Haddad <39592320+vimahaddad@users.noreply.github.com>
2018-12-04 17:13:11 +0100

    Update PaiementBillet.java

--- src/test/java/CucumberJoueur/TP3/PaiementBillet.java
+++ src/test/java/CucumberJoueur/TP3/PaiementBillet.java
@@ -5,19 +5,4 @@ public class PaiementBillet {
 	public double acheterBillet(TypeBillet typeBillet) {
 		return typeBillet.calculCoutBillet();
 	}
-	
-	public static void main(String[] args)
-	{
-		BilletClassic  classic = new BilletClassic();
-		BilletVip      vip     = new BilletVip()    ;
-		PaiementBillet billet  = new PaiementBillet();
-		System.out.println(" ------------------------------------ ");  
-		System.out.println("|         Test Pattern Strategy      |");
-		System.out.println(" ------------------------------------ ");  
-		System.out.println("");
-		System.out.println("        "+billet.acheterBillet(classic));
-		System.out.println("");
-		System.out.println("        "+billet.acheterBillet(vip));
-		System.out.println("");
-	}
 }
