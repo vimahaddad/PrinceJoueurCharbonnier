b7c71dac3e671d7138b9b090f49ceffb5094297c
Vima Haddad <39592320+vimahaddad@users.noreply.github.com>
2018-12-04 17:12:06 +0100

    Update BilletClassic.java

--- src/test/java/CucumberJoueur/TP3/BilletClassic.java
+++ src/test/java/CucumberJoueur/TP3/BilletClassic.java
@@ -3,9 +3,7 @@ package CucumberJoueur.TP3;
 public class BilletClassic implements TypeBillet {
 
 	public double calculCoutBillet() {
-		
-	   System.out.println("<------calcul billet Classic ----->");
-	   System.out.println(" ");
+
 	   return 50;
 	}
 
