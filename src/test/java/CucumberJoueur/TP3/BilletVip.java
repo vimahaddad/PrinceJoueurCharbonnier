7e03702740cbe6385be97e6f6cfd16be648b8f66
Vima Haddad <39592320+vimahaddad@users.noreply.github.com>
2018-12-04 17:12:32 +0100

    Update BilletVip.java

--- src/test/java/CucumberJoueur/TP3/BilletVip.java
+++ src/test/java/CucumberJoueur/TP3/BilletVip.java
@@ -3,9 +3,7 @@ package CucumberJoueur.TP3;
 public class BilletVip implements TypeBillet {
 
 	public double calculCoutBillet() {
-		
-		System.out.println("<-------calcul billet Vip ------>");
-		System.out.println(" ");
+
 		return 145;
 	}
 	
